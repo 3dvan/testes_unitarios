d094e1f8c78a7dca36d6c84dfad5c680345d508f
3dvan <edvanjr2@gmail.com>
2023-04-15 10:41:39 -0300

    testando com o pytest

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -12,3 +12,23 @@ class TestClass:
         resultado = funcionario_teste.idade()
 
         assert resultado == esperado #Then-desfecho
+
+    def test_quando_sobrenome_recebe_Manoel_Edvan_deve_retornar_Carvalho(self):
+        entrada = 'Manoel Edvan'
+        esperado = 'Edvan'
+
+        edvan = Funcionario(entrada, '11/08/1998', 2000)
+        resultado = edvan.sobrenome()
+
+        assert resultado == esperado
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        salario: 100000 #given
+        entrada_nome = 'João Yamato'
+        esperado: 90000
+
+        funcionario_teste = Funcionario('Teste', '11/08/1998', salario)
+        funcionario_teste.decrescimo_salario()   #when
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
